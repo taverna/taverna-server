8417b450612cb148093945157be4c4be01aa857d
Donal Fellows <donal.k.fellows@manchester.ac.uk>
2015-04-14 14:32:47 +0100

    Workaround for awful bug in Jersey client.

--- server-client/src/main/java/uk/org/taverna/server/client/DirEntry.java
+++ server-client/src/main/java/uk/org/taverna/server/client/DirEntry.java
@@ -1,8 +1,12 @@
 package uk.org.taverna.server.client;
 
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.net.URI;
+import java.util.Map;
 
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.uri.internal.JerseyUriBuilder;
 import org.taverna.server.client.wadl.TavernaServer.Root.RunsRunName.Wd.Path;
 import org.taverna.server.client.wadl.TavernaServer.Root.RunsRunName.Wd.Path2;
 import org.taverna.server.client.wadl.TavernaServer.Root.RunsRunName.Wd.Path3;
@@ -26,10 +30,30 @@ public abstract class DirEntry extends Connected {
 	protected DirEntry(Run run, String path) {
 		this.run = run;
 		this.path = trim(path);
-		List<String> elems = Arrays.asList(this.path.split("/"));
-		path1 = run.run.wd().path("").setPath(elems);
-		path2 = run.run.wd().path2("").setPath(elems);
-		path3 = run.run.wd().path3("").setPath(elems);
+		path1 = run.run.wd().path(this.path);
+		path2 = run.run.wd().path2(this.path);
+		path3 = run.run.wd().path3(this.path);
+		UriBuilder ub = new JerseyUriBuilder() {
+			@Override
+			public URI buildFromMap(Map<String, ?> values) {
+				return buildFromMap(values, false);
+			}
+		};
+		try {
+			Field f;
+			f = path1.getClass().getDeclaredField("_uriBuilder");
+			f.setAccessible(true);
+			f.set(path1, ub);
+			f = path2.getClass().getDeclaredField("_uriBuilder");
+			f.setAccessible(true);
+			f.set(path2, ub);
+			f = path3.getClass().getDeclaredField("_uriBuilder");
+			f.setAccessible(true);
+			f.set(path3, ub);
+		} catch (SecurityException | NoSuchFieldException
+				| IllegalArgumentException | IllegalAccessException e) {
+			throw new RuntimeException("failed to install UriBuilder", e);
+		}
 	}
 
 	public void delete() throws ClientException, ServerException {
